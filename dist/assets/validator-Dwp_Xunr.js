2723b5a2fee6d43717c92066ab647c8b5a016403
sunyp1103126-max <sunyp1103126-max@github.com>
2026-04-18 23:56:34 +0800

    Change router to hash mode

---
+++ dist/assets/validator-Dwp_Xunr.js
@@ -0,0 +1,1 @@
+import{a$ as i}from"./index-5HUzeB-D.js";const n=o=>["",...i].includes(o);export{n as i};
--- dist/assets/validator-gyOgJ4pb.js
+++
@@ -1,1 +0,0 @@
-import{a$ as i}from"./index-BbxKIAnt.js";const n=o=>["",...i].includes(o);export{n as i};
